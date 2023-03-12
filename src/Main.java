8be9a296de496587d089fa9056af4b753d38ea3a
steven <theyoung2002@naver.com>
2023-03-12 22:35:50 +0900

    init

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main(new String[]{"Main"});
+    }
+    float[] randomCounts;
+
+    @Override
+    public void settings() {
+        size(640,360);
+    }
+
+    @Override
+    public void setup(){
+        randomCounts = new float[20];
+    }
+
+    @Override
+    public void draw(){
+        background(255);
+
+        int index = (int)random(randomCounts.length);
+        randomCounts[index]++;
+
+        stroke(0);
+        strokeWeight(2);
+        fill(127);
+
+        int w = width / randomCounts.length;
+
+        for(int x = 0; x < randomCounts.length; x++){
+            rect(x*w, height - randomCounts[x],w-1,randomCounts[x]);
+        }
+    }
+}
